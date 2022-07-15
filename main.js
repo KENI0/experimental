842d097fd2b4d40bf0183be008dbf1fb291f3e92
KENI0 <107785434+KENI0@users.noreply.github.com>
2022-07-15 21:55:35 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+const boxEls = document.querySelectorAll('.box');
+boxEls.forEach(function(boxEl){
+    boxEl.classList.add('New');
+})
+//-------------------------------------------------------------//
+
+
