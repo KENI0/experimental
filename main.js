46acb76d4a8ab68eb1fabf29657091be8d2eed55
KENI0 <107785434+KENI0@users.noreply.github.com>
2022-07-21 16:52:37 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -5,4 +5,20 @@ gsap.to(fadeEl, .5 ,{
   delay: (index + 1) * .7 , 
   opacity: 1,
 })
-})
+});
+
+const boxDis = document.querySelector('.box-tag .box-img')
+
+window.addEventListener('scroll',_.throttle(function() {
+  console.log(window.scrollY);
+  if(window.scrollY > 600){
+    gsap.to(boxDis, .5, {
+      opacity: 0
+    })
+  }
+  else {
+    gsap.to(boxDis, .5, {
+      opacity: 1
+    })
+  }
+},300))
