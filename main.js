868901d2763b057d1d1ef575ebe0aaff19216a46
KENI0 <107785434+KENI0@users.noreply.github.com>
2022-07-27 23:47:56 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,24 +1,4 @@
-const fadeEls = document.querySelectorAll('.container .fade-in')
-
-fadeEls.forEach(function(fadeEl,index) {
-gsap.to(fadeEl, .5 ,{
-  delay: (index + 1) * .7 , 
-  opacity: 1,
+const upScroll = document.querySelector('.up-scroll')
+upScroll.addEventListener('click',function() {
+  
 })
-});
-
-const boxDis = document.querySelector('.box-tag .box-img')
-
-window.addEventListener('scroll',_.throttle(function() {
-  console.log(window.scrollY);
-  if(window.scrollY > 600){
-    gsap.to(boxDis, .5, {
-      opacity: 0
-    })
-  }
-  else {
-    gsap.to(boxDis, .5, {
-      opacity: 1
-    })
-  }
-},300))
